885d900919cffc3bb62b0ee6c9f5dbf322565589
Baptiste Briel <baptiste@sa-studio.fr>
2016-04-03 02:28:48 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,32 @@
+# canvas-video
+
+[![stable](http://badges.github.io/stability-badges/dist/stable.svg)](http://github.com/badges/stability-badges)
+
+draws video in canvas element using requestAnimationFrame
+
+## Usage
+
+[![NPM](https://nodei.co/npm/canvas-video.png)](https://www.npmjs.com/package/canvas-video)
+
+`npm install canvas-video --save`
+
+```javascript
+import CanvasVideo from 'canvas-video'
+
+const canvas = new CanvasVideo({
+    video: document.querySelector('video'),
+    canvas: document.querySelector('canvas')
+})
+
+canvas.play()
+```
+
+## methods
+
+- `play`: start drawing video in canvas using requestAnimationFrame
+- `pause`: cancelAnimationFrame
+- `resize`: apply css styles (top, left, width, height) of `<video>` element to `<canvas>`
+
+## License
+
+MIT, see [LICENSE.md](http://github.com/BaptisteBriel/canvas-video/blob/master/LICENSE.md) for details.
---
+++ index.js
@@ -0,0 +1,48 @@
+import raf from 'raf'
+
+export default class Video {
+    
+    constructor(opt) {
+
+        this.video = opt.video
+        this.canvas = opt.canvas
+        this.context = this.canvas.getContext('2d')
+
+        this.rAF = null
+        this.playing = false
+    }
+    
+    play() {
+        
+        this.rAF = raf(this.draw.bind(this))
+        this.playing = true
+    }
+    
+    pause() {
+        
+        raf.cancel(this.rAF)
+        this.playing = false
+    }
+    
+    draw() {
+        
+        if(!this.playing) return
+        
+        this.context.drawImage(this.video, 0, 0, this.bounding.width, this.bounding.height)
+        this.rAF = raf(this.draw.bind(this))
+    }
+    
+    resize() {
+
+        this.bounding = this.video.getBoundingClientRect()
+        
+        this.canvas.style.position = 'absolute'
+        this.canvas.style.top = `${this.bounding.top}px`
+        this.canvas.style.left = `${this.bounding.left}px`
+        this.canvas.style.width = `${this.bounding.width}px`
+        this.canvas.style.height = `${this.bounding.height}px`
+        
+        this.canvas.width = this.bounding.width
+        this.canvas.height = this.bounding.height
+    }
+}
