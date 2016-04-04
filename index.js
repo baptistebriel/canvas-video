d5ea74be80864bd5e9991441093c18fb8c4167b9
Baptiste Briel <baptiste@sa-studio.fr>
2016-04-05 00:15:39 +0200

    fps option

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import raf from 'raf'
+import performance from 'performance-now'
 
 export default class Video {
     
@@ -8,12 +9,23 @@ export default class Video {
         this.canvas = opt.canvas
         this.context = this.canvas.getContext('2d')
         
+        this.fps = opt.fps || 24
+        this.interval = 1000/this.fps
+
+        this.perfs = {}
+
         this.rAF = null
         this.playing = false
     }
     
     play() {
         
+        this.perfs = {
+            now: undefined,
+            then: performance(),
+            delta: undefined
+        }
+
         this.rAF = raf(this.draw.bind(this))
         this.playing = true
     }
@@ -22,14 +34,25 @@ export default class Video {
         
         raf.cancel(this.rAF)
         this.playing = false
+        
+        delete this.perfs.now
+        delete this.perfs.then
     }
     
     draw() {
         
         if(!this.playing) return
         
-        this.context.drawImage(this.video, 0, 0, this.bounding.width, this.bounding.height)
         this.rAF = raf(this.draw.bind(this))
+        
+        this.perfs.now = performance()
+        this.perfs.delta = this.perfs.now - this.perfs.then
+        
+        if (this.perfs.delta > this.interval) {
+
+            this.context.drawImage(this.video, 0, 0, this.bounding.width, this.bounding.height)
+            this.perfs.then = this.perfs.now - (this.perfs.delta % this.interval)
+        }
     }
     
     resize() {
